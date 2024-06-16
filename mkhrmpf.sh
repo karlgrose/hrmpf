da71c7c5ac1624eb2225242729e45c3c728ce53a
Karl R Grose <karlgrose@gromar.org>
2024-06-16 13:27:20 -0700

    specify linux version 6.8

--- mkhrmpf.sh
+++ mkhrmpf.sh
@@ -34,6 +34,7 @@ chmod 0755 hrmpf-include/usr/bin/void-installer
 
 ./mklive.sh \
 	-T "hrmpf live/rescue system" \
+	-v linux6.8 \
 	-C "loglevel=6 printk.time=1 consoleblank=0" \
 	-r https://repo-fastly.voidlinux.org/current \
 	-r https://repo-fastly.voidlinux.org/current/nonfree \
